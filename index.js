d896c80ce8086907a83e4dec1588eb34523e0aca
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-02 14:25:36 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function(event){
+  console.log("Dom is ready");
+
+
+// var cars = ["BMW","Mercedes","Audi"];
+//
+// $(cars).each(function(a,cars){
+//   console.log(a,cars);
+// })
+
+$("#count").html('5').css('color','green';
+
+$('li').addClass('done');
+
+
+
+
+})
